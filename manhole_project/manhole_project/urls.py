ae094a25b7d395b691c4f2be3503380df756d0be
sky1768205 <Tinchurina.dinara@mail.ru>
2025-07-09 15:32:58 -0700

    /

--- manhole_project/manhole_project/urls.py
+++ manhole_project/manhole_project/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from products import views  # Явный импорт views из приложения products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')),
-       path('catalog/manholes/', views.manholes_catalog, name='manholes-catalog'),
-    path('catalog/inspection-chambers/', views.inspection_chambers, name='inspection-chambers'),
-    path('catalog/drainage/', views.drainage, name='drainage'),
-    path('catalog/channels/', views.channels, name='channels'),
+    path('', include('products.urls')),  # Подключение других URL
 ]
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
