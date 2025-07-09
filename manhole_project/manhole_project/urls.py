4678a243d18fb53b20b6a3501e63171826692906
sky1768205 <Tinchurina.dinara@mail.ru>
2025-07-09 15:12:42 -0700

    home

--- manhole_project/manhole_project/urls.py
+++ manhole_project/manhole_project/urls.py
@@ -4,6 +4,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
+       path('catalog/manholes/', views.manholes_catalog, name='manholes-catalog'),
+    path('catalog/inspection-chambers/', views.inspection_chambers, name='inspection-chambers'),
+    path('catalog/drainage/', views.drainage, name='drainage'),
+    path('catalog/channels/', views.channels, name='channels'),
 ]
 from django.conf import settings
 from django.conf.urls.static import static
